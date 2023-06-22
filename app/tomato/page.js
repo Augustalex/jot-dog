1d425c3d0770dbb9e5466b3b2632181f1bb1d0ee
August Alexandersson <augustalexandersson@gmail.com>
2023-06-22 09:15:54 +0200

    more tomatoes

--- app/tomato/page.js
+++ app/tomato/page.js
@@ -2,11 +2,13 @@ import styles from './page.module.css'
 import { kv } from '@vercel/kv';
 
 export default async function Tomato() {
-    const visitorCount = await kv.incr("visitors")
+  const visitorCount = await kv.incr("visitors")
 
   return (
     <main className="main tomato">
-        <div className={styles.test}>TOMATO {visitorCount}</div>
+      <div className={styles.tomatoMeter}>
+        {"🍅".repeat(visitorCount)}
+      </div>
     </main>
   )
 }
