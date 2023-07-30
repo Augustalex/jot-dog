a2e09ac500c51062d2fb42519aac7c3b289f24ee
August Alexandersson <augustalexandersson@gmail.com>
2023-07-30 19:23:53 +0200

    Removed cursors

--- app/notes/components/NotesEntry.tsx
+++ app/notes/components/NotesEntry.tsx
@@ -4,10 +4,7 @@ import React, { useMemo } from "react";
 import styles from "./notes-entry.module.css";
 import { ClientSwrConfig } from "./ClientSwrConfig";
 import { NoteFile } from "../utils/file-utils";
-import { Cursors } from "./cursors/Cursors";
 import dynamic from "next/dynamic";
-import { saveFile } from "../db/file";
-import debounce from "lodash/debounce";
 import { fileClient } from "../db/fileClient";
 
 const Editor = dynamic(() => import("./editor/Editor"), { ssr: false });
@@ -21,11 +18,9 @@ export default function NotesEntry({
   content: Uint8Array;
   localId: string;
 }) {
-  const saveFileSoon = useMemo(
-    () =>
-      debounce(async (file: NoteFile, content: Uint8Array) => {
-        await fileClient.saveBinaryData(file, content);
-      }, 5000),
+  const persist = useMemo(
+    () => (file: NoteFile, content: Uint8Array) =>
+      fileClient.saveBinaryData(file, content),
     []
   );
 
@@ -37,12 +32,8 @@ export default function NotesEntry({
             file={file}
             localId={localId}
             serverContent={content}
-            persist={async (newContent) => {
-              console.log("Calling debounced save file...");
-              await saveFileSoon(file, newContent);
-            }}
+            persist={(newContent) => persist(file, newContent)}
           />
-          {/*<Cursors file={file} localId={localId} />*/}
         </div>
       </main>
     </ClientSwrConfig>
