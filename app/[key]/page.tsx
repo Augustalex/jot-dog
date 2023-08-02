0b9f2c6f0c584595f8914a90f313569853aa27a3
August Alexandersson <augustalexandersson@gmail.com>
2023-08-02 12:43:49 +0200

    /new now creates a new file

--- app/[key]/page.tsx
+++ app/[key]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NotesEntry from "../notes/components/NotesEntry";
-import { getOrCreateFile } from "../notes/db/files";
+import { createFile, getOrCreateFile } from "../notes/db/files";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { fileClient } from "../notes/db/fileClient";
@@ -13,11 +13,20 @@ export default async function Notes({
   };
 }) {
   const localId = cookies().get("local-id")?.value;
-  if (!localId) redirect(`/guest?redirect-to=/notes/${params.key}`);
+  if (!localId) redirect(`/guest?redirect-to=/${params.key}`);
 
-  const file = await getOrCreateFile(params.key);
-  const content = await fileClient.getBinaryFile(file);
-  return (
-    <NotesEntry file={file} content={content as Uint8Array} localId={localId} />
-  );
+  if (params.key === "new") {
+    const file = await createFile();
+    return redirect(`/${file.key}`);
+  } else {
+    const file = await getOrCreateFile(params.key);
+    const content = await fileClient.getBinaryFile(file);
+    return (
+      <NotesEntry
+        file={file}
+        content={content as Uint8Array}
+        localId={localId}
+      />
+    );
+  }
 }
