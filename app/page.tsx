0a72318458ae3216913170f1cdd3a310b6d03ff6
August Alexandersson <augustalexandersson@gmail.com>
2023-08-04 09:38:29 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const responses = [
     "Nothing to see here.",
     "Where you looking for something specific?",
-    'Wrong place at the "' + new Date().toLocaleTimeString() + '" time.',
+    // 'Wrong place at the "' + new Date().toLocaleTimeString() + '" time.',
     "The right website lies within, not on a screen. Especially not on your screen.",
     "This is not the right website for you, but look at the person to your right and tell them you found the right website for them.",
   ];
