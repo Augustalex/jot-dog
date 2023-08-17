ab8c7deaaee45615786f62fe5afd085800f4a638
August Alexandersson <augustalexandersson@gmail.com>
2023-08-17 20:56:46 +0200

    New icon

--- app/[key]/test-login/LoginModal.tsx
+++ app/[key]/test-login/LoginModal.tsx
@@ -2,7 +2,7 @@ import styles from "./login-to-note.module.css";
 import React, { useRef, useState } from "react";
 import { IBM_Plex_Mono } from "next/font/google";
 import Image from "next/image";
-import lockIcon from "../../lock.svg";
+import openLock from "../../open_lock.svg";
 import hiddenIcon from "../../hidden.svg";
 import shownIcon from "../../shown.svg";
 import { NoteFile } from "../../notes/utils/file-utils";
@@ -68,7 +68,7 @@ export function LoginModal({ file }: HistoryModalProps) {
           onClick={login}
           disabled={password.length === 0 || status === "success"}
         >
-          <Image src={lockIcon.src} alt="Lock" width={42} height={42} />
+          <Image src={openLock.src} alt="Lock" width={42} height={42} />
           <span>Login to /{file.key}</span>
         </button>
       </div>
