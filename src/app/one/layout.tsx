428911d7d327e733c877ac532054dbe9c71ceee9
August Alexandersson <augustalexandersson@gmail.com>
2024-06-22 00:07:42 +0200

    fix

--- src/app/one/layout.tsx
+++ src/app/one/layout.tsx
@@ -8,7 +8,11 @@ export const metadata = {
   description: "Jot down notes with your team",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={noto.className}>{children}</body>
