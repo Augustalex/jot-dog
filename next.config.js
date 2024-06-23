5e01f2345a7cda5c1e05aac1fab167360fa0606e
August Alexandersson <augustalexandersson@gmail.com>
2024-06-23 12:27:28 +0200

    Allow cors on the edge

--- next.config.js
+++ next.config.js
@@ -14,20 +14,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
+        // matching all routes
         source: "*",
         headers: [
-          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          // {
-          //   key: "Access-Control-Allow-Methods",
-          //   value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          // },
-          // {
-          //   key: "Access-Control-Allow-Headers",
-          //   value:
-          //     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          // },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ];
