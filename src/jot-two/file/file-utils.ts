db1c7e23a1be16dab4b264eca08d293b1011d8da
August Alexandersson <augustalexandersson@gmail.com>
2024-07-07 18:43:44 +0200

    remove unused code

--- src/jot-two/file/file-utils.ts
+++ src/jot-two/file/file-utils.ts
@@ -1,5 +1,3 @@
-import slugify from "slugify";
-
 export enum FileType {
   Text = "",
   YDoc = ":ydoc:",
@@ -10,26 +8,4 @@ export interface JotTwoFile {
   name: string;
   key: string;
   fileType: FileType;
-}
-
-export function generateFileDetails(files: JotTwoFile[]) {
-  const isoDate = new Date().toISOString();
-  const dateString = isoDate.substring(0, isoDate.indexOf("T"));
-  const newNameTemplate = `Note ${dateString}`;
-  const similarNamesCount = files.filter((f) =>
-    f.name.startsWith(newNameTemplate),
-  ).length;
-  const newName =
-    similarNamesCount > 0
-      ? `${newNameTemplate} (${similarNamesCount})`
-      : newNameTemplate;
-
-  const newKey = slugify(Date.now().toString(), { lower: true });
-  const newFile: JotTwoFile = {
-    name: newName,
-    key: newKey,
-    fileType: FileType.YDoc,
-  };
-
-  return newFile;
 }
